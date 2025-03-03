b88e781495322f5d44eaca0e90e9be627ced1c88
Tercio Almeida <tercio.coritiba@gmail.com>
2025-03-03 20:10:50 -0300

    Fazendo uma animação simples para quando a planta é atingida

--- props/plants/plant.gd
+++ props/plants/plant.gd
@@ -1,10 +1,16 @@
 class_name Plant extends Node2D
 
 @onready var hit_box: HitBox = $HitBox
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	hit_box.Damaged.connect( TakeDamage )
 	pass
 
 func TakeDamage( _hurt_box : HurtBox ) -> void:
+	animation_player.play("plant-killed")
+	animation_player.animation_finished.connect(plant_killed_animation_finished)
+
+func plant_killed_animation_finished( anim : String ) -> void: 
+	print("[DEBUG] plan queue free called! " + anim)
 	queue_free()
